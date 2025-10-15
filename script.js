9fecb357113d64ffb94a6ad0c663a3346ea63f01
Rohit Singh Bisht <rohitsinghbisht568@gmail.com>
2025-10-15 16:26:23 +0530

    add 6 more question in DSA MCQs questions

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const question = [
     {
-        question: "Which of the following is true about the Data Structure :-",
+        question: "Which of the following is true about the data structure :-",
         answer: [
             { text: "Organization of data items in main memory", correct: false},
             { text: "Organization of data items in secondary memory", correct: false},
@@ -10,30 +10,54 @@ const question = [
         ]
     },
     {
-        question: "Which is largest animal in the world?",
+        question: "Which of the following is a non-linear data structure :- ",
         answer: [
-            { text: "shark", correct: false},
-            { text: "Blue whale", correct: true},
-            { text: "Elephant", correct: false},
-            { text: "Giraffe", correct: false},
+            { text: "linked list", correct: false},
+            { text: "array", correct: false},
+            { text: "tree", correct: true},
+            { text: "all of the above", correct: false},
+            { text: "none of the these", correct: false},
         ]
     },
     {
-        question: "Which is largest animal in the world?",
+        question: "Which of the following is a primitive data structure :-",
         answer: [
-            { text: "shark", correct: false},
-            { text: "Blue whale", correct: true},
-            { text: "Elephant", correct: false},
-            { text: "Giraffe", correct: false},
+            { text: "pointer", correct: false},
+            { text: "array", correct: true},
+            { text: "list", correct: false},
+            { text: "all of the above", correct: false},
+            { text: "none of the these", correct: false},
         ]
     },
     {
-        question: "Which is largest animal in the world?",
+        question: "An array is not a suitable data structure for which of the following operation :- ",
         answer: [
-            { text: "shark", correct: false},
-            { text: "Blue whale", correct: true},
-            { text: "Elephant", correct: false},
-            { text: "Giraffe", correct: false},
+            { text: "searching", correct: false},
+            { text: "sorting", correct: false},
+            { text: "deletion", correct: true},
+            { text: "traveral", correct: false},
+            { text: "all of these", correct: false},
+
+        ]
+    },
+    {
+        question: "Which is the following is a most commonly used data structure is the implementation of a DBMS :- ",
+        answer: [
+            { text: "linked list", correct: false},
+            { text: "array", correct: false},
+            { text: "integer", correct: true},
+            { text: "stack", correct: false},
+        ]
+    },
+    {
+        question: "Which is the following is an application of data structure :- ",
+        answer: [
+            { text: "statistical analysis of packages", correct: false},
+            { text: "simulation", correct: false},
+            { text: "operating system", correct: true},
+            { text: "graphics", correct: false},
+            { text: "all of these", correct: false},
+
         ]
     }
 ];
